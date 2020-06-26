4148c29ef3794e10f1a45a808e6a69437b790ab8
ThomasPeruch <peruchthomas@gmail.com>
2020-06-26 19:02:25 -0300

    findByDepartment implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -64,7 +67,6 @@ public class SellerDaoJDBC implements SellerDao {
 		finally {
 			DB.closeStatement(pst);
 			DB.closeResultSet(rs);
-			
 		}
 	}
 
@@ -75,6 +77,7 @@ public class SellerDaoJDBC implements SellerDao {
 		obj.setEmail(rs.getString("Email"));
 		obj.setBaseSalary(rs.getDouble("BaseSalary"));
 		obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setDepartment(dep);
 		return obj;
 	}
 
@@ -90,6 +93,46 @@ public class SellerDaoJDBC implements SellerDao {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
 
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		try {
+			pst = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE department.Id = ? "
+					+ "ORDER BY Name");
+			
+			pst.setInt(1, department.getId());
+			
+			rs = pst.executeQuery();
+			
+			List<Seller> list = new ArrayList<>();
+			Map<Integer,Department> map = new HashMap<>();
+			
+			while(rs.next()) {
+				
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				if(dep==null) {
+					dep = instantiateDepartment(rs);	
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+					
+				Seller obj = instantiateSeller(rs,dep);
+				list.add(obj);
+			}
+			return list;
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(pst);
+			DB.closeResultSet(rs);
+			
+		}
+	}
 }
