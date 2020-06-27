8aec23f28d50b1dcd2b936ec44cd7227287ad340
ThomasPeruch <peruchthomas@gmail.com>
2020-06-27 17:54:00 -0300

    DepartmentDaoJDBC implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
@@ -16,6 +17,34 @@ public class Program {
 		Scanner sc =new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+
+		System.out.println("==TEST 1: department findById==");
+		Department dep = departmentDao.findById(3);
+		System.out.println(dep);	
+		
+		System.out.println("==TEST 2: department findAll==");
+		List<Department>list = departmentDao.findAll();
+		for(Department obj : list) {
+			System.out.println(obj);
+		}
+		
+		System.out.println("==TEST 3: department insert==");
+		Department newDep = new Department(null, "Books");
+		departmentDao.insert(newDep);
+		System.out.println("Inserted! New id = " + newDep.getId());
+		
+		System.out.println("==TEST 4: department update==");
+		dep = departmentDao.findById(6);
+		dep.setName("Music");
+		departmentDao.update(dep);
+		System.out.println("Update Completed");
+		
+		System.out.println("==TEST 5: department delete==");
+		System.out.println("Enter id for delete test :");
+		departmentDao.deleteById(sc.nextInt());
+		System.out.println("Delete completed");
 		
 		System.out.println("==TEST 1: seller findById==");
 		Seller seller = sellerDao.findById(3);
@@ -25,14 +54,14 @@ public class Program {
 		
 		System.out.println("==TEST 2: seller findByDepartment==");
 		Department department = new Department (2, null);
-		List<Seller> list = sellerDao.findByDepartment(department);
-		for(Seller obj : list) {
+		List<Seller> listSeller = sellerDao.findByDepartment(department);
+		for(Seller obj : listSeller) {
 			System.out.println(obj);
 		}
 		
 		System.out.println("==TEST 3: seller findAll==");
-		list = sellerDao.findAll();
-		for(Seller obj : list) {
+		listSeller = sellerDao.findAll();
+		for(Seller obj : listSeller) {
 			System.out.println(obj);
 		}
 		
